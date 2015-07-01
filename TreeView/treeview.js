217b0a60d88f3f7c857f90794eb477c055283cd5
Jim <jim.cosgrave.web@gmail.com>
2015-06-30 22:25:04 -0500

    TreeView Updates
    Updated the treeview plugin so you can pass in some options like effect,
    effect speed and linkOnlyToggle.  LinkOnlyToggle determines if it is
    only the link that triggers a toggle or if the entire li should.

--- TreeView/treeview.js
+++ TreeView/treeview.js
@@ -1,5 +1,4 @@
 /*
- *  Project: Edlong
  *  Description: Small plugin to turn a list of nested ul into a tree view
  *  Author: Jim Cosgrave
  */
@@ -9,6 +8,9 @@
   var pluginName = 'TreeView',
       document = window.document,
       defaults = {
+      	effect: 'toggle',
+      	effectSpeed: '',
+      	linkOnlyToggle: true
       };
 
   // The actual plugin constructor
@@ -18,20 +20,48 @@
 
     this.options = $.extend( {}, defaults, options) ;
 
+    this.validateOptions();
+
     this._defaults = defaults;
     this._name = pluginName;
 
     this.init();
   }
 
+  Plugin.prototype.validateOptions = function () {
+  	if(!this.options.effect || this.options.effect === '') {
+  		this.options.effect = 'toggle';
+  	}
+  }
+
   Plugin.prototype.init = function () {
+    var self = this, $body = $('body');
+
     this.$element.find('ul').eq(0).find('li').find('ul').hide();
 
-    $('body').on('click', 'a.tree-node-toggle', function() {
-        $(this).closest('li').find('ul').eq(0).toggle();
+    $body.off('click').on('click', 'a.tree-node-toggle', function(e) {
+        self.toggle(this, e)
     });
 
+    var li = this.$element.find('li');
+
+    if (!this.options.linkOnlyToggle) {
+	    li.off('click').on('click', function(e) {
+			self.toggle(this, e)
+	    });
+
+        li.css('cursor', 'default');
+	    li.has('ul').css('cursor', 'pointer');
+	}
+
+
   };
+
+  Plugin.prototype.toggle = function(elem, e){
+      $(elem).closest('li').find('ul').eq(0)[this.options.effect](this.options.effectSpeed);
+      e.preventDefault();
+      e.stopPropagation(); 
+  }
 
   $.fn[pluginName] = function ( options ) {
     return this.each(function () {
